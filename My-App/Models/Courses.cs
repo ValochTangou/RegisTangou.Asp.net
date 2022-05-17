e3d6330c415768c9ca1e1e05893d03bc94d6f40b
Regis Tangou <rtangou@gmail.com>
2022-05-17 21:57:30 +0100

    first commit

---
+++ My-App/Models/Courses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace My_App.Models
+{
+    public class Courses
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? grades { get; set; }
+        public ICollection<Students>? Students{ get; set; }
+    }
+}
---
+++ My-App/Models/EntityframeContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace My_App.Models
+{
+    public class EntityframeContext : DbContext
+    {
+        public EntityframeContext(DbContextOptions<EntityframeContext> options)
+              : base(options)
+        {
+
+        }
+        public DbSet<Students> Students { get; set; }
+        public DbSet<Teachers> Teachers { get; set; }
+        public DbSet<Courses> Courses { get; set; }
+
+
+
+    }
+
+}
