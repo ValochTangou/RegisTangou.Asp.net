7b0b1698aa86d35e03ba31b6099224d63ea542ad
Regis Tangou <rtangou@gmail.com>
2022-05-30 21:09:33 +0100

    Adding and configuring the startup.cs

---
+++ My-App/Startup.cs
@@ -0,0 +1,39 @@
+﻿namespace CreatingStartupClass
+{
+    public static class Startup
+    {
+        public static WebApplication InitializeApp(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            configureServices(builder);
+            var app = builder.Build();
+            configure(app);
+            return app;
+
+        }
+
+        private static void configureServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+        }
+
+        private static void configure(WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
